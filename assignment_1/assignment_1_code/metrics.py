c43f626406b9c81e0d1fb729f531709172133187
Anna Laczkó <79013743+annalaczko@users.noreply.github.com>
2025-05-11 11:30:24 +0200

    Init

---
+++ assignment_1/assignment_1_code/metrics.py
@@ -0,0 +1,90 @@
+from abc import ABCMeta, abstractmethod
+import torch
+
+
+class PerformanceMeasure(metaclass=ABCMeta):
+    """
+    A performance measure.
+    """
+
+    @abstractmethod
+    def reset(self):
+        """
+        Resets internal state.
+        """
+
+        pass
+
+    @abstractmethod
+    def update(self, prediction: torch.Tensor, target: torch.Tensor):
+        """
+        Update the measure by comparing predicted data with ground-truth target data.
+        Raises ValueError if the data shape or values are unsupported.
+        """
+
+        pass
+
+    @abstractmethod
+    def __str__(self) -> str:
+        """
+        Return a string representation of the performance.
+        """
+
+        pass
+
+
+class Accuracy(PerformanceMeasure):
+    """
+    Average classification accuracy.
+    """
+
+    def __init__(self, classes) -> None:
+        self.classes = classes
+
+        self.reset()
+
+    def reset(self) -> None:
+        """
+        Resets the internal state.
+        """
+        self.correct_pred = {classname: 0 for classname in self.classes}
+        self.total_pred = {classname: 0 for classname in self.classes}
+        self.n_matching = 0  # number of correct predictions
+        self.n_total = 0
+
+    def update(self, prediction: torch.Tensor, target: torch.Tensor) -> None:
+        """
+        Update the measure by comparing predicted data with ground-truth target data.
+        prediction must have shape (batchsize,n_classes) with each row being a class-score vector.
+        target must have shape (batchsize,) and values between 0 and c-1 (true class labels).
+        Raises ValueError if the data shape or values are unsupported.
+        [len(prediction.shape) should be equal to 2, and len(target.shape) should be equal to 1.]
+        """
+
+        ## TODO implement
+        pass
+
+    def __str__(self):
+        """
+        Return a string representation of the performance, accuracy and per class accuracy.
+        """
+
+        ## TODO implement
+        pass
+
+    def accuracy(self) -> float:
+        """
+        Compute and return the accuracy as a float between 0 and 1.
+        Returns 0 if no data is available (after resets).
+        """
+
+        ## TODO implement
+        pass
+
+    def per_class_accuracy(self) -> float:
+        """
+        Compute and return the per class accuracy as a float between 0 and 1.
+        Returns 0 if no data is available (after resets).
+        """
+        ## TODO implement
+        pass
