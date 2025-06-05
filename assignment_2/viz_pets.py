fb09161f49d7e881cef7e75d3c23e2bc7a5d6890
annalaczko <79013743+annalaczko@users.noreply.github.com>
2025-06-06 00:00:13 +0200

    Update viz_pets.py

--- assignment_2/viz_pets.py
+++ assignment_2/viz_pets.py
@@ -34,7 +34,13 @@ if __name__ == '__main__':
         v2.ToDtype(torch.long, scale=False),
         v2.Resize(size=(64,64), interpolation=v2.InterpolationMode.NEAREST)
     ])
-    
+    train_data = OxfordPetsCustom(root="/data/", 
+                            split="trainval",
+                            target_types='segmentation', 
+                            transform=input_transform,
+                            target_transform=target_transform,
+                            download=True)
+
     val_data = OxfordPetsCustom(
         root="data", 
         split="test", 
@@ -45,10 +51,11 @@ if __name__ == '__main__':
     )
     val_loader = torch.utils.data.DataLoader(val_data, batch_size=4, shuffle=False, num_workers=2)
 
-    model_path = Path("saved_models") / "model.pth"
+    model_path = Path("saved_models") / "FCN_model_FCN.pth"
 
     model = fcn_resnet50()
-    model.load_state_dict(torch.load(model_path))  # csak a súlyokat töltjük be
+    model.classifier[4] = torch.nn.Conv2d(512, len(train_data.classes_seg), kernel_size=1)
+    model.load_state_dict(torch.load(model_path))
 
 
     # model = torch.load(model_path)
@@ -56,11 +63,11 @@ if __name__ == '__main__':
 
     with torch.no_grad():
         images, true_masks = next(iter(val_loader))
-        outputs = model(images)
-        predicted_masks = torch.argmax(outputs, dim=1).unsqueeze(1).float()  # (B,1,H,W) a megjelenítéshez
+        outputs = model(images)['out']
+        predicted_masks = torch.argmax(outputs, dim=1).unsqueeze(1).float()
 
     input_grid = torchvision.utils.make_grid(images, nrow=4)
-    pred_mask_grid = torchvision.utils.make_grid(predicted_masks / predicted_masks.max(), nrow=4)  # normalizálva
+    pred_mask_grid = torchvision.utils.make_grid(predicted_masks / predicted_masks.max(), nrow=4) 
 
     os.makedirs("img", exist_ok=True)
     imshow(input_grid, filename="img/val_input_images.png")
